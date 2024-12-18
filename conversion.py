c655e0bcb082175d43dd401a3519958de280221e
EddyBolan <fonkoutchinmouo@yahoo.fr>
2024-12-18 10:48:12 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,27 @@
+# *********** AUTO GRATED TASK 2 **********
+
+# =============== Pseudocode ==========
+# ==== Declare variables
+# num1 = 99.23; num2 = 23; num3 = 150; string1 = "100"
+# ==== Conversion
+# Convert num1 to an INTEGER and store it to num1_to_int
+# Convert num2 to a FLOAT and store it to num2_to_float
+# Convert num3 to a STRING and assign it to num3_to_str
+# Convert string1 to an INTEGER and assign it to string1_to_int
+# ==== Print out all the variables
+# Print out all the variables: num1_to_int, num2_to_float, num3_to_str, string1_to_int 
+
+# =============== Declare Variables =========
+num1 = 99.23; num2 = 23; num3 = 150; string1 = "100"
+
+# =============== Conversion ================
+num1_to_int = int(num1)          # Convert num1 to an INTEGER
+num2_to_float = float(num2)      # Convert num2 to a FLOAT
+num3_to_str = str(num3)          # Convert num3 to a STRING
+string1_to_int = int(string1)    # Convert string1 to an INTEGER
+
+# == Print out all the variables on separate lines
+print(f"num1_to_int = {num1_to_int}")          # Print num1_to_int
+print(f"num2_to_float = {num2_to_float}")      # Print num2_to_float
+print(f"num3_to_str = {num3_to_str}")          # Print num3_to_str
+print(f"string1_to_int = {string1_to_int}")    # Print string1_to_int
---
+++ hello_world.py
@@ -0,0 +1,19 @@
+# *********** AUTO GRATED TASK 1 **********
+
+# =============== Pseudocode ==========
+# request the user to enter their name
+# store the user’s name using input()
+# request the user to enter their age
+# store the user’s age using input()
+# Output the user's name and age 
+# Output the string “Hello World!”
+
+# =============== The user's name and age ===============
+user_name = input("Enter your name: ")  
+user_age = input("Enter your age: ")  
+
+# =============== Print the user's name and age =========
+print(f"{user_name} is {user_age} years old.")     
+
+# =============== Print the string “Hello World!” =======      
+print("Hello World!")  
